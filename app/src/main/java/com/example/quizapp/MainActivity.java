49c62216f5c8afcd17dd063575baddff1b46418a
MarHan123 <m_hansen1993@hotmail.com>
2024-04-03 20:10:55 +0200

    Fixing Gallery

--- app/src/main/java/com/example/quizapp/MainActivity.java
+++ app/src/main/java/com/example/quizapp/MainActivity.java
@@ -7,7 +7,6 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 
 import com.example.quizapp.model.ImageEntity;
 import com.example.quizapp.viewmodel.ImageViewModel;
@@ -21,19 +20,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         imageViewModel = new ViewModelProvider(this).get(ImageViewModel.class);
-
-        Button galleryButton = findViewById(R.id.gallery_btn);
-        Button quizButton = findViewById(R.id.play_btn);
-
-        galleryButton.setOnClickListener(v -> {
-            startActivity(new Intent(this, GalleryActivity.class));
-        });
-
-        quizButton.setOnClickListener(v -> {
-            startActivity(new Intent(this, QuizActivity.class));
-        });
-
         startPictures();
+    }
+
+    public void galleryButton(View view) {
+        Intent intent = new Intent(this, GalleryActivity.class);
+        startActivity(intent);
+    }
+
+    public void playButton(View view) {
+        Intent intent = new Intent(this, QuizActivity.class);
+        startActivity(intent);
     }
 
     private void startPictures() {
